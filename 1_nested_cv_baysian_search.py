88ab7b4da7e3bc32a39836ec31eaff94291aa764
mathcom <mathcombio@yonsei.ac.kr>
2022-04-20 16:12:08 +0900

    FIX: fix the parameter name of skopt.gp_minize because 'n_random_starts' is deprecated in skopt 0.8 and replaced by 'n_initial_points'

--- 1_nested_cv_baysian_search.py
+++ 1_nested_cv_baysian_search.py
@@ -5,10 +5,11 @@ import numpy as np
 import pandas as pd
 import skopt
 from datetime import datetime
-from scipy.spatial.distance import pdist, squareform
 from sklearn.metrics import accuracy_score, average_precision_score, roc_auc_score
 from sklearn.model_selection import StratifiedKFold, train_test_split
-from refdnn import REFDNN
+from refdnn.model import REFDNN
+from refdnn.dataset import DATASET
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -43,7 +44,6 @@ def main():
         print('[ARGUMENT] RESPONSEFILE: {}'.format(args.responseFile))
         print('[ARGUMENT] EXPRESSIONFILE: {}'.format(args.expressionFile))
         print('[ARGUMENT] FINGERPRINTFILE: {}'.format(args.fingerprintFile))
-        print('[ARGUMENT] GPUUSE: {}'.format(args.gpuuse))
         print('[ARGUMENT] OUTPUTDIR: {}'.format(args.o))
         print('[ARGUMENT] NUMBAYESIANSEARCH: {}'.format(args.s))
         print('[ARGUMENT] OUTERKFOLD: {}'.format(args.k))
@@ -140,7 +140,7 @@ def main():
         search_result = skopt.gp_minimize(func=fitness,
                                           dimensions=dimensions_hyperparameters,
                                           n_calls=numbayesiansearch,
-                                          n_random_starts=1,
+                                          n_initial_points=3, # 'n_random_starts' is deprecated in skopt 0.8 and replaced by 'n_initial_points'
                                           acq_func='EI',
                                           noise=1e-10,
                                           verbose=0)
@@ -323,110 +323,6 @@ def fitness(hyperparameters):
     return -training_accuracy
 
 
-class DATASET:
-    def __init__(self, drfile, gefile, fpfile):
-        ## 1. Read data
-        ## 1-1) Gene expression
-        self.ge = pd.read_csv(gefile, sep=',', index_col=0)
-        ## 1-2) Drug response
-        self.dr = pd.read_csv(drfile, sep=',', dtype='str')
-        self.DRUGKEY = self.dr.columns[0]
-        self.CELLKEY = self.dr.columns[1]
-        self.LABELKEY = self.dr.columns[2]
-        ## 1-3) Fingerprint
-        self.fp = pd.read_csv(fpfile, sep=',', index_col=0).transpose()
-        
-        ## 2. Preprocessing
-        ## 2-1) Find targets
-        target_drugs = self._find_target_drugs()
-        target_cells = self._find_target_cells()
-        ## 2-2) Filter data
-        self.ge = self.ge.filter(target_cells)
-        self.dr = self.dr[self._get_target_idx(target_drugs, target_cells)]
-        ## 2-3) Label string to integer
-        idx = self.dr[self.LABELKEY] == 'Resistance'
-        self.dr[self.LABELKEY][idx] = 1
-        self.dr[self.LABELKEY][~idx] = 0
-        self.dr[self.LABELKEY] = self.dr[self.LABELKEY].astype(np.uint8)
-        
-        ## 3. Structural Similarity Profile
-        self.SSP = self._make_SSP()
-
-    def __len__(self):
-        return len(self.dr)
-        
-    def get_drugs(self, unique=False):
-        return self._get_series(self.DRUGKEY, unique)
-        
-    def get_cells(self, unique=False):
-        return self._get_series(self.CELLKEY, unique)
-        
-    def get_labels(self, unique=False):
-        return self._get_series(self.LABELKEY, unique)
-        
-    def get_genes(self):
-        return self.ge.index.values
-        
-    def get_exprs(self):
-        return self.ge.values.T # per cell
-    
-    def make_xdata(self, idx=None):
-        if idx is None:
-            cells = self.get_cells()
-        else:
-            cells = self.get_cells()[idx]
-        return np.array([self.ge[cell].values for cell in cells], dtype=np.float32)
-    
-    def make_ydata(self, idx=None):
-        if idx is None:
-            labels = self.get_labels()
-        else:
-            labels = self.get_labels()[idx]
-        return np.expand_dims(np.array(labels, dtype=np.uint8), axis=-1)
-    
-    def make_sdata(self, base_drugs, idx=None):
-        SSP = self.SSP.filter(base_drugs, axis='index')
-        if idx is None:
-            drugs = self.get_drugs()
-        else:
-            drugs = self.get_drugs()[idx]
-        return np.array([SSP[drug].values for drug in drugs], dtype=np.float32)
-        
-    def make_idata(self, base_drugs, idx=None):
-        drug2idx = {drug:i for i, drug in enumerate(base_drugs)}
-        if idx is None:
-            drugs = self.get_drugs()
-        else:
-            drugs = self.get_drugs()[idx]
-        return np.array([drug2idx[drug] for drug in drugs], dtype=np.int64)
-    
-    def _make_SSP(self):
-        ssp_mat = 1. - squareform(pdist(self.fp.values.T, 'jaccard'))
-        return pd.DataFrame(ssp_mat, index=self.fp.columns, columns=self.fp.columns)
-        
-    def _get_series(self, KEY, unique):
-        if unique:
-            return np.sort(self.dr[KEY].unique(), kind='mergesort')
-        else:
-            return self.dr[KEY].values
-    
-    def _find_target_drugs(self):
-        target_drugs = []
-        self.drug2fp = {}
-        for drugname in self.dr[self.DRUGKEY].unique():
-            if drugname in self.fp:
-                target_drugs.append(drugname)
-                self.drug2fp[drugname] = self.fp[drugname].astype(np.uint8).astype(bool)
-        return target_drugs
-        
-    def _find_target_cells(self):
-        return self.ge.columns.intersection(self.dr[self.CELLKEY].unique())
-
-    def _get_target_idx(self, target_drugs, target_cells):
-        idx_drugs = self.dr[self.DRUGKEY].isin(target_drugs)
-        idx_cells = self.dr[self.CELLKEY].isin(target_cells)
-        return idx_drugs & idx_cells    
-    
     
     
 if __name__=="__main__":
